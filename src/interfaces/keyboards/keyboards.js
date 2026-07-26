7b3cab87ea38e4d6c29ea533378d2c8d6bbc706b
ifiverr6-netizen <ifiverr6@gmail.com>
2026-07-26 23:47:40 +0600

    Update keyboards.js

--- src/interfaces/keyboards/keyboards.js
+++ src/interfaces/keyboards/keyboards.js
@@ -3,31 +3,31 @@ const { getCourseProducts, getSubscriptionProducts } = require('../../domain/pro
 
 function mainMenuKeyboard() {
   return Markup.inlineKeyboard([
-    [Markup.button.callback('📚 Digital Courses', 'view_courses')],
-    [Markup.button.callback('⭐ Premium Subscription', 'view_subs')],
-    [Markup.button.callback('📦 My Orders', 'my_orders')],
+    [Markup.button.callback('📚 কোর্সসমূহ', 'view_courses')],
+    [Markup.button.callback('⭐ সাবস্ক্রিপশন', 'view_subs')],
+    [Markup.button.callback('📦 আমার অর্ডার', 'my_orders')],
     [Markup.button.callback('❓ FAQ', 'faq')],
-    [Markup.button.callback('🎧 Live Support', 'support')]
+    [Markup.button.callback('🎧 সাপোর্ট', 'support')]
   ]);
 }
 
 function backToMenuKeyboard() {
-  return Markup.inlineKeyboard([[Markup.button.callback('🔙 Main Menu', 'main_menu')]]);
+  return Markup.inlineKeyboard([[Markup.button.callback('🔙 ফিরে যান', 'main_menu')]]);
 }
 
 function coursesKeyboard() {
-  const buttons = getCourseProducts().map(p => [
-    Markup.button.callback(p.title, `buy_${p.code}`)
+  const buttons = getCourseProducts().map((p, i) => [
+    Markup.button.callback(`${i + 1}. ${p.title}`, `buy_${p.code}`)
   ]);
-  buttons.push([Markup.button.callback('🔙 Main Menu', 'main_menu')]);
+  buttons.push([Markup.button.callback('🔙 ফিরে যান', 'main_menu')]);
   return Markup.inlineKeyboard(buttons);
 }
 
 function subsKeyboard() {
-  const buttons = getSubscriptionProducts().map(p => [
-    Markup.button.callback(p.title, `buy_${p.code}`)
+  const buttons = getSubscriptionProducts().map((p, i) => [
+    Markup.button.callback(`${i + 1}. ${p.title}`, `buy_${p.code}`)
   ]);
-  buttons.push([Markup.button.callback('🔙 Main Menu', 'main_menu')]);
+  buttons.push([Markup.button.callback('🔙 ফিরে যান', 'main_menu')]);
   return Markup.inlineKeyboard(buttons);
 }
 
@@ -35,7 +35,8 @@ function productActionsKeyboard() {
   return Markup.inlineKeyboard([
     [Markup.button.callback('💳 পেমেন্ট শুরু করুন', 'start_payment')],
     [Markup.button.callback('📋 অর্ডার করার নিয়ম', 'submit_trx')],
-    [Markup.button.callback('🔙 Main Menu', 'main_menu')]
+    [Markup.button.callback('📞 সাপোর্ট', 'support')],
+    [Markup.button.callback('🔙 ফিরে যান', 'main_menu')]
   ]);
 }
 
